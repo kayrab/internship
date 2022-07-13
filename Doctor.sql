38de5fcc5827909e6b76dd1652bc353e5d8edaeb
kayrab <50419086+kayrab@users.noreply.github.com>
2022-07-13 10:27:05 +0300

    Add files via upload

---
+++ Doctor.sql
@@ -0,0 +1,11 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [id]
+      ,[first_name_doctor]
+      ,[surname_doctor]
+      ,[age]
+      ,[phone_number]
+      ,[office]
+  FROM [Hospital].[dbo].[Doctor]
+
+INSERT INTO Doctor(first_name_doctor, surname_doctor, age, phone_number, office) VALUES('Pesho', 'Petrov', 56, '0893587175', 'Pirogov');
+INSERT INTO Doctor(first_name_doctor, surname_doctor, age, phone_number, office) VALUES('Georgi', 'Tasev', 40, '0893587975', 'Pirogov');
---
+++ Patient.sql
@@ -0,0 +1,21 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [id]
+      ,[first_name]
+      ,[surname]
+      ,[age]
+      ,[phone_number]
+      ,[previous_illnesses]
+      ,[allergies]
+  FROM [Hospital].[dbo].[Patient]
+
+INSERT INTO Patient(first_name, surname, age, phone_number, previous_illnesses, allergies) VALUES('Georgi', 'Boiadjiev', 14, '0893535675', 'cold, covid', 'dogs');
+INSERT INTO Patient(first_name, surname, age, phone_number, previous_illnesses, allergies) VALUES('Kayra', 'Boiadjieva', 17, '0893587175', 'cold', NULL);
+INSERT INTO Patient(first_name, surname, age, phone_number, previous_illnesses, allergies) VALUES('Mariq', 'Ivanova', 20, '0883467717', 'covid', NULL);
+
+
+SELECT p.first_name, p.surname, p.age, p.phone_number, 
+p.previous_illnesses, p.allergies, d.first_name_doctor, d.surname_doctor, 
+v.visited_at, v.diagnosis
+FROM Patient p
+LEFT JOIN Visits v ON v.patient_id = p.id
+LEFT JOIN Doctor d ON v.doctor_id = d.id;
